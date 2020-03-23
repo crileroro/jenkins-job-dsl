a35fac5519b3e662d713aeb19ab595abc2d8105c
Cristhian <cristhian.roa87@gmail.com>
2020-03-23 17:16:29 +0100

    Updated maven groovy script

--- jobs/maven_project.groovy
+++ jobs/maven_project.groovy
@@ -1,4 +1,13 @@
 job('Maven-project-dsl'){
+    scm{
+        git{
+            remote{
+                url('https://github.com/crileroro/java-maven.git')
+            }
+
+            branch('master')
+        }
+    }
     steps{
         shell('********** Starting build **********')
     }

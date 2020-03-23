d02e9d0c5d899dd14a32fe4ae17e3bc85838429d
Cristhian <cristhian.roa87@gmail.com>
2020-03-23 19:50:36 +0100

    Modified groovy script names

--- jobs/remote_connection.groovy
+++ jobs/remote_connection.groovy
@@ -6,7 +6,7 @@ job('remote-connection-dsl'){
     }
     steps{
         remoteShell('remote_user@remote _host:22'){
-            command('USER=$(whoami)', 'echo "This is $USER from $hostname"')
+            command('USER=$(whoami)', 'echo "This is $USER from $(hostname)"')
         }
     }
 

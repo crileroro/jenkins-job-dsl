cd64bd464c22ed65ae2fbf6f6b250c6fc8ddf355
Cristhian <cristhian.roa87@gmail.com>
2020-03-27 17:00:57 +0100

    Modified remote host file

--- jobs/remote_connection.groovy
+++ jobs/remote_connection.groovy
@@ -1,5 +1,8 @@
 job('remote-connection-dsl'){
     steps{
+        publishOverSsh{
+            server('remote _host@remote_user:22')
+        }
         remoteShell('remote_user@remote _host:22'){
             command('USER=$(whoami)', 'echo "This is $USER from $(hostname)"')
         }

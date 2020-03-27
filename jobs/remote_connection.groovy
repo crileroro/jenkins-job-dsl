a145b4fb0b58131ff160421dcb2f21375e10df82
Cristhian <cristhian.roa87@gmail.com>
2020-03-27 17:19:43 +0100

    Reverted file remote_connection

--- jobs/remote_connection.groovy
+++ jobs/remote_connection.groovy
@@ -1,8 +1,5 @@
 job('remote-connection-dsl'){
     steps{
-        publishOverSsh{
-            server('remote _host@remote_user:22')
-        }
         remoteShell('remote_user@remote _host:22'){
             command('USER=$(whoami)', 'echo "This is $USER from $(hostname)"')
         }
